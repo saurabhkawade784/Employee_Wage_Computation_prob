a506811e6528decf28fab1b80d36b3c96f89801b
saurabhkawade784 <saurabhkawade784@gmail.com>
2020-09-27 16:26:05 +0530

    Calculating employee daily wage

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -10,7 +10,13 @@ if [[ $IS_EMPLOYEE_PRESENT -eq $Random_No_Check ]];
 	then
 		#If Random_No_Check value is '1'.
 		echo " Employee is present"
+		Wage_Per_Hour=20;
+		Full_Day_Hour=8;
+		#calculate total salary of employee
+		Salary_Of_Employee=$(( $Wage_Per_Hour * $Full_Day_Hour ))
 	else
 		#If random_No_Check value is '0'.
 		echo " Employee is absent"
+		Salary_Of_Employee=0;
 fi
+echo "Your salary is creadited to your account,Rupees" $Salary_Of_Employee
