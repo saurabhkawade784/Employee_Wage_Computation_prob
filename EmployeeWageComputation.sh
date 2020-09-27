77053a41d9e35d45f861c49684f252c3558614c9
saurabhkawade784 <saurabhkawade784@gmail.com>
2020-09-27 16:40:47 +0530

    Calculate full and part time employee wage

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -4,19 +4,33 @@ echo "Welcome To EmployeeWageComputaion program"
 #Set "1" to indicate employee is present.
 #Constant
 IS_EMPLOYEE_PRESENT=1;
+IS_EMPLOYEE_FULLTIME=1;
+IS_EMPLOYEE_PARTTIME=0;
+WAGE_PER_HOUR=20;
 #Variable
 Random_No_Check=$(($RANDOM%2));
 if [[ $IS_EMPLOYEE_PRESENT -eq $Random_No_Check ]];
-	then
-		#If Random_No_Check value is '1'.
-		echo " Employee is present"
-		Wage_Per_Hour=20;
-		Full_Day_Hour=8;
-		#calculate total salary of employee
-		Salary_Of_Employee=$(( $Wage_Per_Hour * $Full_Day_Hour ))
-	else
-		#If random_No_Check value is '0'.
-		echo " Employee is absent"
-		Salary_Of_Employee=0;
+then
+		#If Random_No_check value is '1'
+		echo "Employee is present"
+		JobType=$(($RANDOM%2))
+		if [[ $IS_EMPLOYEE_FULLTIME -eq $JobType ]]
+		then
+			#If JobType value is '1'.
+			echo "This is Full Time Employee"
+			Day_Hour=8;
+		else
+			echo "This is Part Time Employee"
+			Day_Hour=4;
+
+
+		fi
+else
+	echo "Employee is absent."
+	Day_Hour=0;
+
 fi
-echo "Your salary is creadited to your account,Rupees" $Salary_Of_Employee
+
+#calculate total salary of employee
+One_Day_Salary_Of_Employee=$(( $WAGE_PER_HOUR * $Day_Hour ))
+echo "Your one day salary is creadited to your account,Rupees" $One_Day_Salary_Of_Employee
