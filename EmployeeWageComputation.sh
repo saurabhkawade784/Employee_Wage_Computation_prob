238372ace8e6bc8f120299c84d048532aa91c26f
saurabhkawade784 <saurabhkawade784@gmail.com>
2020-09-27 16:53:11 +0530

    Employee wage program using case statements

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -8,28 +8,20 @@ IS_EMPLOYEE_FULLTIME=1;
 IS_EMPLOYEE_PARTTIME=0;
 WAGE_PER_HOUR=20;
 #Variable
-Random_No_Check=$(($RANDOM%2));
-if [[ $IS_EMPLOYEE_PRESENT -eq $Random_No_Check ]];
-then
-		#If Random_No_check value is '1'
-		echo "Employee is present"
-		JobType=$(($RANDOM%2))
-		if [[ $IS_EMPLOYEE_FULLTIME -eq $JobType ]]
-		then
-			#If JobType value is '1'.
-			echo "This is Full Time Employee"
-			Day_Hour=8;
-		else
-			echo "This is Part Time Employee"
-			Day_Hour=4;
+Random_No_Check=$(($RANDOM%3));
+case $Random_No_Check in
+	$IS_EMPLOYEE_FULLTIME)
+			     echo "This is Full Time Employee"
+			     Day_Hour=8;
+			     ;;
+	$IS_EMPLOYEE_PARTTIME) echo "This is Part Time Employee"
+			     Day_Hour=4;
+			     ;;
+		           *) echo "Employee is absent."
+		             Day_Hour=0;
+			     ;;
 
-
-		fi
-else
-	echo "Employee is absent."
-	Day_Hour=0;
-
-fi
+esac
 
 #calculate total salary of employee
 One_Day_Salary_Of_Employee=$(( $WAGE_PER_HOUR * $Day_Hour ))
